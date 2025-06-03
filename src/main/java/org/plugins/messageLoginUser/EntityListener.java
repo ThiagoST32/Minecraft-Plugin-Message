70106ad4050f61cfa7e06795b07a2e1f93a2d360
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-06-03 15:23:38 -0400

    fetch (update) function change 'PlayerLoginEvent' to 'PlayerJoinEvent'

--- src/main/java/org/plugins/messageLoginUser/EntityListener.java
+++ src/main/java/org/plugins/messageLoginUser/EntityListener.java
@@ -3,13 +3,12 @@ package org.plugins.messageLoginUser;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerCommandSendEvent;
-import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class EntityListener implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR)
-    public void onUserLoginServer(PlayerLoginEvent event){
+    public void onUserLoginServer(PlayerJoinEvent event){
         event.getPlayer().sendMessage("Bem-vindo ao servidor");
     }
 }
